7ed945c4cb08ca3338b4f2fbd1c39d37d1f1dcb8
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2021-01-23 02:07:09 +0600

    final

--- assignment.js
+++ assignment.js
@@ -1,32 +1,39 @@
+//kiloToMeter Problem
+
 function kilometerToMeter(kilometer){
     var result = kilometer * 1000;
     if(kilometer < 0){
         return("Distance can not be negative")
     }
     else{
-        return result;
+        return("Distance(in meter):" +  result) ;
     }
+        
 }
 var distance = kilometerToMeter(2);
-// console.log(distance);
+ console.log(distance);
+
+// budgetCalculator Problem
 
 function budgetCalculator(watch,clock,laptop){
     totalCost = watch *50 + clock * 100 + laptop * 500; 
     if(watch< 0){
-        return("can not negative");
+        return("Quantity can not negative");
     }
     else if (clock < 0){
-        return("can not negative");
+        return("Quantity can not negative");
     }
     else if(laptop < 0){
-        return ("can not negative");
+        return ("Quantity can not negative");
     }
     else{
-        return totalCost;
+        return("Your Total Cost is:" + totalCost ) ;
     }
 }
-var cost = budgetCalculator(3,1,1);
-// console.log(cost);
+var cost = budgetCalculator(3,-1,1);
+ console.log(cost);
+
+//hotelCost Problem
 
 function hotelCost (totalDays){
     if (totalDays < 0){
@@ -35,14 +42,14 @@ function hotelCost (totalDays){
     
      else if(totalDays > 0 && totalDays <= 10){
         totalCost = totalDays * 100;
-        return totalCost;
+        return ("Your Total Bill is:" + totalCost);
     }
     else if(totalDays > 10 && totalDays <= 20){
         var firstPart = 10 * 100;
         var remaining = totalDays - 10;
         var secondPart = remaining * 80;
         var totalCost = firstPart + secondPart;
-        return totalCost; 
+        return ("Your Total Bill is:" + totalCost);
     }
     else if(totalDays > 20){
         var firstPart = 10 * 100;
@@ -50,7 +57,7 @@ function hotelCost (totalDays){
         var remaining = totalDays - 20;
         var thirdPart = remaining * 50;
         var totalCost = firstPart + secondPart + thirdPart;
-        return totalCost;
+        return ("Your Total Bill is:" + totalCost);
         }
         else{
             return("Enter valid Number");
@@ -58,18 +65,25 @@ function hotelCost (totalDays){
         
         
 }
-var days = hotelCost(2530);
-// console.log(days);
+var days = hotelCost(15);
+ console.log(days);
+
+ // megaFriend Problem
 
 function megaFriend(friendsName){
-    var max.length = friendsName[0];
-    for(var i = 0; i < friendsName.length; i++);{
-        var element.length = friendsName[i];
+    
+    var max = friendsName[0];
+   
+    for(var i = 0; i < friendsName.length; i++){
+         var element = friendsName[i];
         if(element.length > max.length){
-            max.length = element.length;
-            return("highest value is:",max);
+            max = element;
+            
         }
+        
     }
+    return max;
 }
-var name1 = megaFriend(['Sakhawat,Ayman,Alvi,None']);
+
+var name1 = megaFriend(['Sakhawa1','Sakhawat222','Alvi','None']);
 console.log(name1);
